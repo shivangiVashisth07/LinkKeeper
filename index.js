2f5653b27d37db7517cde024439a4d10f3d7bf65
Shivangi <shivangivashishth07@gmail.com>
2023-07-23 19:10:07 +0530

    Feat: Add delete all button

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 const inputBtn = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
 let myLeads = [];
 
 // using local-storage
 
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
   renderLeads();
 }
 
+deleteBtn.addEventListener("dblclick", () => {
+  localStorage.clear();
+  myLeads = [];
+  renderLeads();
+});
+
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = "";
   localStorage.setItem("myLeads", JSON.stringify(myLeads));
   renderLeads();
-
-  console.log(localStorage.getItem("myLeads"));
 });
 
 function renderLeads() {
